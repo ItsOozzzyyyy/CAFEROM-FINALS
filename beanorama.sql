e4baabf4827917b5208917bd0f7cc07ead67e64e
ItsOozzzyyyy <ozzymozzy569@gmail.com>
2026-03-19 23:02:10 +0800

    Create beanorama.sql

---
+++ beanorama.sql
@@ -0,0 +1,11 @@
+USE beanorama;
+
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer VARCHAR(100),
+    category VARCHAR(100),
+    item VARCHAR(100),
+    size VARCHAR(50),
+    quantity INT,
+    total DECIMAL(10,2)
+);
